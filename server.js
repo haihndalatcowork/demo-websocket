7ef77421bb10dfe61c861cce3cea2730d0817e20
haihn <haihn1@dalatcowork.com>
2019-06-22 09:15:54 +0700

    add socketIO

--- server.js
+++ server.js
@@ -1,4 +1,12 @@
 const app = require('express')();
+const socketIO = require('socket.io');
+
+socketIO.on("connection", socket => {
+    console.log("connected");
+    socket.on("disconnect", () => {
+        console.log("disconnected");
+    })
+});
 
 const port = process.env.PORT || 5000;
 
